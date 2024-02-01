72004222b2fde06f23853e86445461da30225b27
Uriah Ahrak <uriah.ahrak@optibus.com>
2024-02-01 22:44:20 +0200

    added redis connection
    Signed-off-by: Uriah Ahrak <uriah.ahrak@optibus.com>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,27 @@
+import dns from "dns/promises";
+import { createClient } from "redis";
+
 const main = async () => {
   console.log("hi");
   console.log("Hello World");
+  console.log("Hello World 2");
+
+  const client = createClient({
+    url: "redis://redis-master",
+  });
+
+  client.on("error", (err) => console.log("Redis Client Error", err));
+
+  await client.connect();
+
+  const result = await dns.lookup("redis-headless", 4);
+  console.log({ result });
+
+  const address = await client.get(result.address);
+  console.log({ address });
+  if (!address) {
+    await client.set(result.address, 1);
+  }
 };
 
 main();
